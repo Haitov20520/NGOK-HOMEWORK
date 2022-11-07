59f857e461855dc1bd7e1ffd3ab785ddbf81c704
Haitov20520 <116105023+Haitov20520@users.noreply.github.com>
2022-11-07 12:52:16 +0700

    Create array.h

---
+++ array.h
@@ -0,0 +1,6 @@
+#ifndef _ARRAY_H_
+#define _ARRAY_H_
+void fill_array(int *array, const unsigned int size);
+void print_array(const int array[], const unsigned int size);
+int search_max(const int array[], const unsigned int size);
+#endif // _ARRAY_H_
