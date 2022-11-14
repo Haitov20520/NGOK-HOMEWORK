d4e6d7fceed177212e6345af0d3286ee45299075
Haitov20520 <116105023+Haitov20520@users.noreply.github.com>
2022-11-14 13:31:07 +0700

    Update array.h

--- array.h
+++ array.h
@@ -3,4 +3,11 @@
 void fill_array(int *array, const unsigned int size);
 void print_array(const int array[], const unsigned int size);
 int search_max(const int array[], const unsigned int size);
+int search_index(const int array[], const unsigned int size, const int number);
+int search_min(const int array[], const unsigned int size);
+void reverse_array(int *array, const unsigned int size);
+void sorting(int *array, const unsigned int size);
+int equal(const int arrayFirst[], const unsigned int sizeFirst,
+          const int arraySecond[], const unsigned int sizeSecond);
+
 #endif // _ARRAY_H_
